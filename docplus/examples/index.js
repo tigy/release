e60829af02d3faf2ee61d93dd5690417a717458b
xuld <xuld@vip.qq.com>
2012-09-07 17:57:38 +0800

    文档

--- docplus/examples/index.js
+++ docplus/examples/index.js
@@ -1,18 +1,47 @@
 jsonp({
-	"menu": {
-		"表单(Form)": {
+	"menu": [
+		{
+			"name": "表单(Form)",
 			"title": "表单输入域",
-			"href": "examples/controls/form/index.html",
-			//"icon": "none",
-			"menu": {
-				"文本框(TextBox)": {
+			"href": "controls/form/index.html",
+			"icon": "none",
+			"menu": [
+				{
+					"name": "文本框(TextBox)",
 					"title": "普通的文本框",
-					"href": "examples/controls/form/textbox.html",
+					"href": "controls/form/textbox.html",
 					//"icon": "components",
-					"thumbnail": "examples/images/textbox.gif",
+					"thumbnail": "images/textbox.gif",
+					"description": "普通的文本框"
+				},
+				{
+					"name": "文本域(TextArea)",
+					"title": "普通的文本框",
+					"href": "controls/form/textarea.html",
+					//"icon": "components",
+					"thumbnail": "images/textarea.gif",
 					"description": "普通的文本框"
 				}
-			}
+			]
+		},
+		{
+			"name": "按钮(Button)",
+			"title": "按钮",
+			"href": "controls/button/index.html",
+			"menu": [
+				{
+					"name": "按钮(Button)",
+					"title": "普通的按钮",
+					"href": "controls/button/button.html",
+					//"icon": "components",
+					"thumbnail": "images/button.gif",
+					"description": "普通的按钮"
+				},
+				{
+					"name": "更多按钮(Button)",
+					"href": "~/controls/button/button.html"
+				}
+			]
 		}
-	}
+	]
 });
